2a472ea2008368230175624c57ec451928f9e3e0
Jason Addy <Jasonxaddy@gmail.com>
2018-12-08 18:46:56 -0600

    FinishedPyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,44 +7,74 @@ csv_path = os.path.join('..', 'Resources', 'budget_data.csv')
 with open(csv_path, 'r', newline='') as csvfile:
     budget = csv.reader(csvfile, delimiter=',')
     
-    # creating empty sets to append to later
-    months = []
-    profit_loss = []
-    change = []
-    diff = []
+    max_profit = 0
+    max_loss = 0
     max_month = []
 
     # skipping headers
     csv_header = next(csvfile)
 
    
-# using <row[0].split(',')[0]> to pull month/year values. I had to copy and paste raw data into Excel to put it into csv format. 
-# using <int(row[0].split(',')[1]) to pull Profit/Loss values because the .csv file did not split at the comma.  
-# Because it didn't split, the numbers weren't formatted as integers. 
-    
+    first_month = next(budget)
+    months = 1
+    total_profit = float(first_month[1])
+    total_diff = 0
+    last_months_profit = float(first_month[1])
     for row in budget:
-        months.append(row[0].split(',')[0])
-        profit_loss.append(int(row[0].split(',')[1]))
+        months += 1
+        current_profit = float(row[1])
+        total_profit += current_profit
+        current_diff = current_profit - last_months_profit
+        total_diff += current_diff
+        last_months_profit = current_profit
+        # print(current_diff)
+        # print (row)
+        if current_diff > max_profit:
+            max_profit = current_diff
+            max_profit_month = row[0]
+        if current_diff < max_loss:
+            max_loss = current_diff
+            max_loss_month = row[0]
 
-    i = 1
-    for i, x in enumerate(profit_loss):
-        diff.append(profit_loss[i] - profit_loss[i-1])
+print(max_profit)
+print(max_loss)
 
+    # print(total_diff)
+
+average_diff = round((total_diff)/(months - 1), 2)
+
+    #print(months)
+    #print(total_profit)
+    #print(months)
+    #print(profit_loss)
+
+   # i = 1
+    # for i, x in enumerate(profit_loss):
+       #diff.append(profit_loss[i] - profit_loss[i-1])
+        # print(i)
+
+
+#max_month = max(row[1])
+#print(max_month)
+
+#print(diff)
 
 print('Financial Analysis')
 print('--------------------------------------------')
 
-print(f'Total Months: {len(months)}')
-print(f'Total: ${sum(profit_loss)}')
+print(f'Total Months: {months}')
+print(f'Total: ${total_profit}')
+print(f'Average Change : ${average_diff}')
+print(f'Greatest Increase in Profits: {max_profit_month} (${max_profit})')
+print(f'Greatest Decrease in Profits: {max_loss_month} (${max_loss})')
+# average = sum(pl)/len(months)
+# print(f'Average Profit: ${average}')
+# print((sum(pl)/len(pl)))
 
-#average = sum(pl)/len(months)
-#print(f'Average Profit: ${average}')
-#print((sum(pl)/len(pl)))
-
-total_diff = diff.pop(0)
-print(f'Average Change: $ {total_diff/len(diff)}')
-print(f'Greatest Increase in Profits: (${max(diff)})')
+# total_diff = diff.pop(0)
+# print(f'Average Change: $ {total_diff/len(diff)}')
+# print(f'Greatest Increase in Profits: (${max(diff)})')
 
 
  
-print(f'Greatest Decrease in Profits: (${min(diff)})')
+# print(f'Greatest Decrease in Profits: (${min(diff)})')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,20 +1,20 @@
 import os 
 import csv
 
-
 csv_path = os.path.join('..', 'Resources', 'election_data.csv')
 
 with open(csv_path, 'r', newline='') as csvfile:
     results = csv.reader(csvfile, delimiter=',')
 
+    csv_header = next(csvfile)
+
     voter_id = []
     county = []
     candidates = []
     
-
-    #skipping headers
-    csv_header = next(csvfile)
-
+    max_profit = 0
+    max_loss = 0
+    max_month = []
 
     for row in results:
         voter_id.append(int(row[0]))
@@ -61,3 +61,85 @@ print(f'{final_candidate_list[3]}: ')
 #    i = 1
    # for i, x in enumerate(profit_loss)
     #    diff.append(profit_loss[i] - profit_loss[i-1])
+
+
+
+
+# import os
+# import csv
+# import pandas as pd
+
+# link = 'https://nu.bootcampcontent.com/NU-Coding-Bootcamp/NUCHI201811DATA2/raw/master/Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv'
+# results = pd.read_csv(link)
+# print(results)
+
+# # Hello
+
+# csv_path = os.path.join('..', 'Resources', 'election_data.csv')
+
+# with open(csv_path, 'r', newline='') as csvfile:
+#     results = csv.reader(csvfile, delimiter=',')
+    
+#     for _ in results:
+#         print('yo, sup?')
+# #     votes_cast = 0
+#     # candidates = {}
+
+#     # skipping headers
+#     #  csv_header = next(results)
+
+#     votes_cast = 0
+#     for row in results:
+#         votes_cast += 1
+#         if row[2] in candidates:
+#             candidates[row[2]] = candidates[row[2]] + 1
+#         else: 
+#             candidates[row[2]] = 1
+
+# print(candidates)
+
+#         # if candidates[row] == 'Khan':
+#         #     Khan_vote_count += 1
+
+
+# # print(votes_cast)
+    
+# #     Khan_vote_count = 0
+# #     Li_vote_count = 0
+# #     Correy_vote_count = 0
+# #     OTooley_vote_count = 0 
+# #     for i, x in enumerate(results):
+# #         if candidates[i] == 'Khan':
+# #             Khan_vote_count += 1
+        
+# # print(Khan_vote_count)
+
+# # Total Votes
+# print('Election Results')
+# print('--------------------------------------------')
+# print(f'Total Votes: {len((voter_id))}')
+# print('--------------------------------------------')
+
+
+# candidate_with_votes = set(candidates)
+# final_candidate_list = list(candidate_with_votes)
+# print(final_candidate_list)
+
+
+# print(f'{final_candidate_list[0]}: ')
+# print(f'{final_candidate_list[1]}: ')
+# print(f'{final_candidate_list[2]}: ')
+# print(f'{final_candidate_list[3]}: ')
+
+
+# #print('Election Results')
+# #print('--------------------------------------------')
+# #print(f'Total Votes: {len(voter_id)}')
+# #print(len(county))
+# #print(f'List of Candidates {candidates}')
+
+
+
+# #    i = 1
+#    # for i, x in enumerate(profit_loss)
+#     #    diff.append(profit_loss[i] - profit_loss[i-1])
